1c0625d83dea63a2a8e112bd3cc55981de9f961e
Len <nbv3698@yahoo.com.tw>
2016-12-21 20:44:27 +0800

    mysorter

--- MySorter.java
+++ MySorter.java
@@ -3,11 +3,18 @@ package ntou.cs.sorter;
 public class MySorter
 {
 	public void sort (double[] d)
-	{
+	{	
+		for (int i = 0; i < d.length; i++){
+			if(d[i] < 0){
+				return;
+			}
+		}
+		
 		for (int i = 0; i < d.length - 1; i++)
 		{
 			for (int j = 0; j < d.length - i - 1; j++)
-			{
+			{	
+				
 				if (d[j] > d[j + 1])
 				{
 					swap(d, j, j + 1);
